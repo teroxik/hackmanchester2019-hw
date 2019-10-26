0eb6b436081da38df7dced35aae867b39d8f675a
Jan Machacek <jan.machacek@gmail.com>
2019-10-26 23:02:01 +0100

    b

---
+++ include/pins.h
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+namespace pins
+{
+constexpr int sunday_green = D0;
+constexpr int sunday_red = -1;
+constexpr int sunday_detect = D3;
+
+constexpr int friday_red = D4;
+constexpr int friday_green = -1;
+constexpr int friday_detect = D5;
+
+constexpr int sms_rx = D1;
+constexpr int sms_tx = D2;
+
+constexpr int vote_0 = D10;
+constexpr int vote_1 = D9;
+// constexpr int vote_2 = D8; // A0??
+constexpr int vote_now = D6;
+}; // namespace pins
