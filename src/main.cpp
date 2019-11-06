d81a4f9c5d2120bd4653d652505bc7591ee509dc
Jan Machacek <jan.machacek@gmail.com>
2019-11-06 11:12:42 +0000

    Sema

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,48 @@
 #include <esp_task_wdt.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
+#include <freertos/semphr.h>
 #include <freertos/task.h>
 #include "sdkconfig.h"
 
+#define INTR
+
+#define MIN_DELAY 10
 #define BUTTON_GPIO GPIO_NUM_17
 #define BLINK_GPIO GPIO_NUM_5
 
-static xQueueHandle gpio_evt_queue = nullptr;
+// static xQueueHandle gpio_evt_queue = nullptr;
+static xSemaphoreHandle semaphore_handle = nullptr;
 
-extern "C" void led_task(void* arg) {
-  uint32_t x = 0;
+void led_task(void* arg) {
+  bool x = false;
   while (true) {
-    if (xQueueReceive(gpio_evt_queue, &x, portMAX_DELAY)) {
-      printf("Recvd\n");
-      if (x == 0)
-        x = 1;
-      else
-        x = 0;
-      gpio_set_level(BLINK_GPIO, x);
-    }
+    xSemaphoreTake(semaphore_handle, portMAX_DELAY);
+    x = !x;
+    gpio_set_level(BLINK_GPIO, x);
+
+    // uint32_t dummy = 0;
+    // if (xQueueReceive(gpio_evt_queue, &dummy, portMAX_DELAY)) {
+    //   x = !x;
+    //   gpio_set_level(BLINK_GPIO, x);
+    // }
   }
 }
 
-extern "C" void IRAM_ATTR button_isr_handler(void* arg) {
-  uint32_t gpio_num = 0;
+void IRAM_ATTR button_isr_handler(void* arg) {
+  // uint32_t gpio_num = 0;
+  // static auto last_time = 0;
+  // auto now = xTaskGetTickCountFromISR();
+  // if (now - last_time > MIN_DELAY) {
+  //   xQueueSendToBackFromISR(gpio_evt_queue, &gpio_num, nullptr);
+  //   last_time = now;
+  // }
+
   static auto last_time = 0;
-  auto now = gettimeofday();
-  if (now - last_time > 1000L) {
-    ets_printf("Intr!\n");
-    xQueueSendToBackFromISR(gpio_evt_queue, &gpio_num, nullptr);
+  auto now = xTaskGetTickCountFromISR();
+  if (now - last_time > MIN_DELAY) {
+    auto ignored = pdFALSE;
+    xSemaphoreGiveFromISR(semaphore_handle, &ignored);
     last_time = now;
   }
 }
@@ -42,15 +55,32 @@ extern "C" void app_main(void) {
   gpio_pad_select_gpio(BUTTON_GPIO);
   gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
   gpio_set_pull_mode(BUTTON_GPIO, GPIO_PULLUP_ONLY);
+#ifdef INTR
   gpio_set_intr_type(BUTTON_GPIO, GPIO_INTR_NEGEDGE);
 
-  gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+  semaphore_handle = xSemaphoreCreateBinary();
+  // gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
   xTaskCreate(led_task, "led_task", 2048, NULL, 10, NULL);
   gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1);
   gpio_isr_handler_add(BUTTON_GPIO, button_isr_handler, nullptr);
-
+#else
+  auto last_time = 0;
+  bool light = false;
+  bool was_high = true;
   while (1) {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    // printf("Tick!\n");
+    auto now = xTaskGetTickCount();
+    if (gpio_get_level(BUTTON_GPIO) == 0) {
+      if (was_high && now - last_time > MIN_DELAY) {
+        last_time = now;
+        light = !light;
+        gpio_set_level(BLINK_GPIO, light);
+      }
+      was_high = false;
+    } else {
+      was_high = true;
+    }
+
+    vTaskDelay(1);
   }
+#endif
 }
